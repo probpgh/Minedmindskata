59f7edd5a14d19f173176811051d4c50735949ed
PRob <PRob>
2017-01-18 09:35:36 -0500

    case loop with substitution

--- case.rb
+++ case.rb
@@ -1,12 +1,12 @@
 for k in 1..100 do
 	
 	case 
+	when (k %3 == 0) && (k %5 == 0)
+		puts "Mined Minds"	
 	when k %3 == 0
 		puts "Mined"
 	when k %5 == 0
 		puts "Minds"
-	when (k %3 == 0) && (k %5 == 0)
-		puts "Mined Minds"
 	else
 		puts k
 	end
